6bba5446c53ab635863f094c6620d8bf5deb3385
SpiritRain <spiritrain@gmail.com>
2015-12-04 14:00:26 +0800

    add any connect

--- client.php
+++ client.php
@@ -39,60 +39,72 @@ include_once 'header.php';
                             </tr>
                              <tr>
                                 <td>4</td>
-                                <td>IOS</td>
-                                <td>shadowsocks</td>
-                                <td><a href="https://itunes.apple.com/tc/app/shadowsocks/id665729974?mt=8" target="_blank">App Store</a></td>
+                                <td>Android</td>
+                                <td>OpenConnect</td>
+                                <td><a href="download/fqrouter.apk" target="_blank">本地下载1</a>/<a href="http://sg.spiritrain.tk/download/fqrouter.apk" target="_blank">本地下载2</a></td>
                             </tr>
-                            <tr>
+                             <tr>
                                 <td>5</td>
-                                <td>Windows</td>
-                                <td>shadowsocks 2.5.8</td>
-                                <td><a href="download/shadowsocks-win-2.5.8.zip" target="_blank">本地下载1</a>/<a href="http://sg.spiritrain.tk/download/shadowsocks-win-2.5.8.zip" target="_blank">本地下载2</a></td>
+                                <td>Android</td>
+                                <td>OpenConnect</td>
+                                <td><a href="https://play.google.com/store/apps/details?id=app.openconnect" target="_blank">Google Play</a></td>
                             </tr>
                             <tr>
                                 <td>6</td>
                                 <td>Windows</td>
                                 <td>shadowsocks 2.5.8</td>
-                                <td><a href="https://github.com/shadowsocks/shadowsocks-csharp/releases/download/2.5.8/Shadowsocks-win-2.5.8.zip" target="_blank">远程下载</a></td>
+                                <td><a href="download/shadowsocks-win-2.5.8.zip" target="_blank">本地下载1</a>/<a href="http://sg.spiritrain.tk/download/shadowsocks-win-2.5.8.zip" target="_blank">本地下载2</a></td>
                             </tr>
                             <tr>
                                 <td>7</td>
-                                <td>Chrome</td>
-                                <td>SwitchySharp插件</td>
-                                <td><a href="download/SwitchySharp.crx" target="_blank">本地下载1</a>/<a href="http://sg.spiritrain.tk/download/SwitchySharp.crx" target="_blank">本地下载2</a></td>
+                                <td>Windows</td>
+                                <td>shadowsocks 2.5.8</td>
+                                <td><a href="https://github.com/shadowsocks/shadowsocks-windows/releases/download/2.5.8/Shadowsocks-2.5.8.zip" target="_blank">远程下载</a></td>
                             </tr>
                             <tr>
                                 <td>8</td>
+                                <td>Google Chrome浏览器</td>
+                                <td>SwitchySharp插件</td>
+                                <td><a href="download/SwitchySharp.tar.gz" target="_blank">本地下载1</a>/<a href="http://sg.spiritrain.tk/download/SwitchySharp.tar.gz" target="_blank">本地下载2</a></td>
+                            </tr>
+                            <tr>
+                                <td>9</td>
+                                <td>iOS</td>
+                                <td>AnyConnect</td>
+                                <td><a href="https://itunes.apple.com/us/app/cisco-anyconnect/id392790924" target="_blank">AppStore</a></td>
+                            </tr>
+                            <tr>
+                                <td>10</td>
                                 <td>Mac OS</td>
                                 <td>shadowsocks 2.6.3</td>
                                 <td><a href="download/shadowsocksX-2.6.3.dmg" target="_blank">本地下载1</a>/<a href="http://sg.spiritrain.tk/download/shadowsocksX-2.6.3.dmg" target="_blank">本地下载2</a></td>
                             </tr>
                              <tr>
-                                <td>9</td>
+                                <td>11</td>
                                 <td>Mac OS</td>
                                 <td>shadowsocks 2.6.3</td>
                                 <td><a href="https://github.com/shadowsocks/shadowsocks-iOS/releases/download/2.6.3/ShadowsocksX-2.6.3.dmg" target="_blank">远程下载</a></td>
                             </tr>
                             <tr>
-                                <td>10</td>
+                                <td>12</td>
                                 <td>Linux</td>
                                 <td>shadowsocks Qt5</td>
                                 <td><a href="https://github.com/shadowsocks/shadowsocks-qt5/wiki/Installation" target="_blank">链接</a></td>
                             </tr>
                              <tr>
-                                <td>11</td>
+                                <td>13</td>
                                 <td>Linux</td>
                                 <td>shadowsocks Command Line</td>
                                 <td>pip install shadowsocks</td>
                             </tr>
                           <tr>
-                                <td>12</td>
+                                <td>14</td>
                                 <td>OpenWRT</td>
                                 <td>shadowsocks-libev</td>
                                 <td>opkg install shadowsocks-libev</td>
                             </tr>
                             <tr>
-                                <td>13</td>
+                                <td>15</td>
                                 <td>OpenWRT</td>
                                 <td>shadowsocks-libev-polarssl</td>
                                 <td>opkg install shadowsocks-libev-polarssl</td>
